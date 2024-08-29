e43f0a2e88026f91d8d4b927c454ed4969550772
kp003919 <muhsin_haji@hotmail.com>
2024-08-29 19:04:24 +0100

    Update Source1.cpp
    Fact function has been added to the file

--- Source1.cpp
+++ Source1.cpp
@@ -1,6 +1,19 @@
 #include<stdio.h>
 #include<iostream>
-
+int fact(int n)
+{
+	if (n <0 )
+	{ 
+		return -1;
+	}
+	else if(n<=1)
+	{
+           
+	}
+	else
+	return n*fact(n-1);
+	
+}
 int main()
 {
 	count << "Hello worl" << endl;
